bd38cf4148c502733c8f4bd096c36fe1cca5b866
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-05-29 21:44:11 -0300

    Atividade

--- AdivinharNumero.java
+++ AdivinharNumero.java
@@ -1,24 +1,18 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class AdivinharNumero {
-    public static void main(string[]args){
-
+    public static void main(String[]args){
         Random  random = new Random();
         Scanner scanner = new Scanner(System.in);
-
-        int numeroQualquer, numeroUsuario = 100;
-
-        numeroQualquer = random.nextInt(bound: 11);
-
-        while(numeroQualquer != numeroUsuario);
-            System.out.print(" Tente acertar o número de 0 a 10");
-            numerousuario = scanner.nextInt();
-
-            if( numeroQualquer == nemeroUsuario);
+        int numeroQualquer; int numeroUsuario = 0;
+        numeroQualquer = random.nextInt(10);
+        while(numeroQualquer!=numeroUsuario); {
+            System.out.print(" Tente acertar o número de 0 a 10:");
+            numeroUsuario = scanner.nextInt();
+        }
+        if (numeroQualquer==numeroUsuario); {
                System.out.println("Você acertou!");
-            } else {
-                System.out.println(" Você errou!");
+               scanner.close();
     }
-    
+    }
 }
