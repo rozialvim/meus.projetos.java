b706a89fbb0d30b749184cbbadc21960eec31632
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-06-27 18:21:14 -0300

    contagempares

---
+++ contagempares.java
@@ -0,0 +1,9 @@
+public class contagempares {
+    public static void main(String[]args){
+            for(int contador = 0; contador <= 20; contador++){
+                if(contador % 2 == 0){
+                    System.out.println(contador);
+                }
+            }
+    }
+}
